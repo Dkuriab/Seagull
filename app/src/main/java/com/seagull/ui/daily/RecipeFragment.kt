dbe947e7e38b6a30be0226b461c43962650d3e74
Dkuriab <62207029+Dkuriab@users.noreply.github.com>
2021-01-22 17:58:30 +0300

    Back button added, minimal preview size changed

--- app/src/main/java/com/seagull/ui/daily/RecipeFragment.kt
+++ app/src/main/java/com/seagull/ui/daily/RecipeFragment.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.transition.MaterialArcMotion
 import com.google.android.material.transition.MaterialContainerTransform
 import com.google.android.material.transition.MaterialSharedAxis
+import com.seagull.MyApplication.Companion.appContext
 import com.seagull.R
 import com.seagull.misc.unHideBottomBar
+import com.seagull.ui.MainActivity
 import com.squareup.picasso.Picasso
 
 class RecipeFragment : Fragment() {
     private lateinit var recipePicture : ImageView
     private lateinit var recipeDescription : TextView
+    private lateinit var backButton : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +38,7 @@ class RecipeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         recipePicture = view.findViewById(R.id.recipe_picture)
         recipeDescription = view.findViewById(R.id.recipe_description)
+        backButton = view.findViewById(R.id.back_button)
         unHideBottomBar()
 
         Picasso.get()
@@ -43,6 +50,13 @@ class RecipeFragment : Fragment() {
             RecipeFragmentArgs.fromBundle(requireArguments()).recipeDescription
 
         setTransition()
+        setListeners()
+    }
+
+    private fun setListeners() {
+        backButton.setOnClickListener{
+            findNavController().navigateUp()
+        }
     }
 
     private fun setTransition() {
